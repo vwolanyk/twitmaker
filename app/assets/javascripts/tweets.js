8d3d5306e9b82cc2674ab0fdad7774be1507be9a
Vlad Wolanyk <vladwolanyk@yahoo.ca>
2017-07-13 22:48:15 -0400

    HTML response appends to twit list

--- app/assets/javascripts/tweets.js
+++ app/assets/javascripts/tweets.js
@@ -1,2 +1,30 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+document.addEventListener("DOMContentLoaded", function(){
+
+
+
+  $(new_tweet).on( "submit", function( event ) {
+  event.preventDefault();
+
+  $.ajax({
+    url:$(this).attr('action'),
+    method:$(this).attr('method'),
+    data: $(this).serialize(),
+    dataType: '',
+
+  }).done(function(responseData){
+    $(tweetList).prepend(responseData);
+    console.log(responseData);
+
+
+  })
+
+});
+
+
+
+
+
+  // END OF DOM CONTENT
+})
--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -7,11 +7,24 @@ class TweetsController < ApplicationController
   def create
     @tweet = Tweet.new(tweet_params)
 
+    if request.xhr?
+      respond_to do |format|
+        format.html do
+          if @tweet.save
+            render partial: 'tweet', locals: {tweet: @tweet} 
+          else
+            # Address errors
+          end
+          end
+        end
+  else
+      @tweet = Tweet.new(tweet_params)
     if @tweet.save
       redirect_to tweets_path
     else
       render :index
     end
+  end
   end
 
   def destroy
@@ -23,3 +36,32 @@ class TweetsController < ApplicationController
     params.require(:tweet).permit(:message)
   end
 end
+
+
+# if request.xhr?
+#       respond_to do |format|
+#         format.html do
+#           if @greeting.save
+#             render partial: 'greeting', locals: {greeting: @greeting}
+#           else
+#             # Address errors
+#           end
+#         end
+#         format.json do
+#           if @greeting.save
+#             render json: @greeting
+#           else
+#             # Address errors
+#           end
+#         end
+#       end
+#     else
+#       @greeting = Greeting.new(greeting_params)
+#       if @greeting.save
+#         redirect_to greetings_path
+#       else
+#         @greetings = Greeting.all.order(created_at: :desc)
+#         render :index
+#         # Add some error handling
+#       end
+#     end
